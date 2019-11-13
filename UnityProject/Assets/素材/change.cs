cd9aad9fa89afd27d4f5e57ff0539213aee86f46
kiki <ccnk3847@gmail.com>
2019-11-13 16:42:57 +0800

    完成期中

---
+++ UnityProject/Assets/素材/change.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class change : MonoBehaviour
+{
+    public Camera ca;
+    public Vector3[] po;
+
+    public void moveca(int i)
+    {
+        ca.transform.position = po[i];
+    }
+
+}
